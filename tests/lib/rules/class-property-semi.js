a222cb6905d919f8b211bd631f0e7aab4013122a
peakchen90 <1558267774@qq.com>
2019-04-10 12:28:00 +0800

    class-property-semi

---
+++ tests/lib/rules/class-property-semi.js
@@ -0,0 +1,84 @@
+const RuleTester = require('eslint').RuleTester
+const rule = require('../../../lib/rules/class-property-semi')
+
+new RuleTester({
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6
+  }
+}).run('class-property-semi', rule, {
+  valid: [
+    {
+      code: `
+        class A {
+          static foo = 'Alice';
+          bar = () => {};
+        }
+      `
+    },
+    {
+      code: `
+        class A {
+          static foo = 'Alice';
+          bar = () => {};
+        }
+      `,
+      options: ['always']
+    },
+    {
+      code: `
+        class A {
+          static foo = 'Alice'
+          bar = () => {}
+        }
+      `,
+      options: ['never']
+    }
+  ],
+  invalid: [
+    {
+      code: `
+        class A {
+          static foo = 'Alice'
+        }
+      `,
+      options: ['always'],
+      errors: [{
+        message: 'Missing semicolon.'
+      }]
+    },
+    {
+      code: `
+        class A {
+          static foo = 'Alice';
+        }
+      `,
+      options: ['never'],
+      errors: [{
+        message: 'Extra semicolon.'
+      }]
+    },
+    {
+      code: `
+        class A {
+          bar = () => {}
+        }
+      `,
+      options: ['always'],
+      errors: [{
+        message: 'Missing semicolon.'
+      }]
+    },
+    {
+      code: `
+        class A {
+          bar = () => {};
+        }
+      `,
+      options: ['never'],
+      errors: [{
+        message: 'Extra semicolon.'
+      }]
+    }
+  ]
+})
